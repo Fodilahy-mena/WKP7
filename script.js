be6ef472a00ffa179bdf820b49807916421a016e
JEROME Emilien <jerome.emi@onja.org>
2020-08-15 17:59:07 +0300

    add new book list when clicking on add button

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const listOfBooks = [
         author: 'Jeritiko',
         genre: 'Fantasy',
         pages: 230,
+        id: Date.now(),
+        read: true,
 
     },
     {
@@ -16,6 +18,8 @@ const listOfBooks = [
         author: 'William',
         genre: 'Story',
         pages: 340,
+        id: Date.now(),
+        read: false,
 
     },
     {
@@ -23,28 +27,29 @@ const listOfBooks = [
         author: 'Vlamire',
         genre: 'Story',
         pages: 140,
+        id: Date.now(),
+        read: true,
 
     },
 ];
 
 
-let myItems = [];
-
 const bookForm = document.querySelector('.book_form');
 const bookList = document.querySelector('.book_list');
+const newBookLists = document.querySelector('.book-lists');
+
 console.log(bookForm, bookList);
 
 const bookLists = () => {
-    // let filterBooks = [...listOfBooks];
     
     const html = listOfBooks.map(book => {
         return `
-        <li class="book_item">
+        <li class="book_item" id="${book.id}">
             <span class="itemTitle">${book.title}</span>
             <span class="itemAuthor">${book.author}</span>
             <span class="itemGenre">${book.genre}</span>
             <span class="itemPages">${book.pages}</span>
-            <input value="book.id" type="checkbox">
+            <input ${book.read ? 'checked' : ''} value="book.id" type="checkbox">
             <button class="delete_btn" aria-label="Remove" value="item.id">✖️</button>
         </li>
         `;
@@ -52,40 +57,69 @@ const bookLists = () => {
     bookList.innerHTML = html;
 }
 
- 
+ let myItems = [];
+const handleToSubmit = e => {
+    // debugger;
 
-// const handleToSubmit = e => {
-//     e.preventDefault();
-//     const title = e.currentTarget.item.value;
-//     console.log(title);
+    e.preventDefault();
+    const form = e.currentTarget;
+    const title = form.title.value;
+    const author = form.author.value;
+    const genre = form.genre.value;
+    const pages = form.pages.value;
+    const read = form.status.value;
+    
+    console.log(title);
+    
+     const item = {
+         title: `${title}`,
+         author: `${author}`,
+         genre: `${genre}`,
+         pages: `${pages}`,
+         id: Date.now(),
+         read: `${read}`,
+          
+     };
 
-//     if (!title) return;
-//      const item = {
-//          title,
-//          id: Date.now(),
-//          complete: false,
-//      };
+     myItems.push(item);
 
-//      myItems.push(item);
-//      console.log(`There are now ${myItems.length} item in your state`);
-//      e.target.reset();
-// };
+     console.log(myItems);
 
-// const displayItems = () => {
-//     console.log(item);
-//     const html = myItems.map(item => `<li class="book_item">
-//     <span class="itemTitle">${item.title}</span>
-//     <span class="itemAuthor">${item.author}</span>
-// 	<span class="itemGenre">${item.genre}</span>
-//     <span class="itemPages">${item.pages}</span>
-//     <input value="${item.id}" ${item.complete ? 'checked' : ''} type="checkbox">
-//     <button class="delete_btn" aria-label="Remove ${item.title}" value="${item.id}">✖️</button>
-//     </li>`).join(' ');
-//     console.log(html);
-//     bookList.innerHTML = html;
-// };
+     e.target.reset();
 
-// bookForm.addEventListener('submit', handleToSubmit);
-// bookList.addEventListener('click', displayItems);
+     bookList.dispatchEvent(new CustomEvent('itemUpdated'));
+    
+};
 
+const newBookList = e => {
+    e.preventDefault();
+    const newhtml = myItems.map(item => `
+        <li class="book_item" id="${item.id}">
+            <span class="itemTitle">${item.title}</span>
+            <span class="itemAuthor">${item.author}</span>
+            <span class="itemGenre">${item.genre}</span>
+            <span class="itemPages">${item.pages}</span>
+            <input ${item.read === "Read" ? 'checked' : ''} value="book.id" type="checkbox">
+            <button class="delete_btn" aria-label="Remove" value="item.id">✖️</button>
+        </li>
+        `).join('');
+        newBookLists.innerHTML = newhtml;
+    // bookList.insertAdjacentHTML('beforeend',newhtml);
+    console.log(bookList);
+}
+
+
+// delete list
+
+const handleDeleteList = (e) => {
+    if(e.target.classList.contains('delete_btn')) {
+        const deleteList = e.target;
+        deleteList.closest('.book_item').remove();
+    }
+}
+
+
+document.addEventListener('click', handleDeleteList);
 window.addEventListener('DOMContentLoaded', bookLists);
+bookForm.addEventListener('submit', handleToSubmit);
+bookList.addEventListener('itemUpdated', newBookList);
